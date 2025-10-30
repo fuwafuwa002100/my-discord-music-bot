12e0966c036c3af8a533eb9838400fc6775a8350
fuwari <c25i037ig@ous.jp>
2025-10-30 14:43:59 +0900

    Add web server for Render Web Service

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-// index.js
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
+
 import dotenv from "dotenv";
 dotenv.config();
 
